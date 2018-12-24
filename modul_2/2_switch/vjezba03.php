faeb7ece48051c62b83fa754fde79b27a65003e3
Edin Jašarević <edinjasar14@gmail.com>
2018-12-24 01:01:46 +0100

    modul 02 | 02 switch | vjezba03

---
+++ modul_2/2_switch/vjezba03.php
@@ -0,0 +1,15 @@
+<?php
+$numberOfEntries = 0;
+
+switch ($numberOfEntries){
+    case 0 :
+        echo ' Welcome! You can start with learning...';
+		break;
+    case 20 :
+        echo ' It is time for test...';
+        break;
+    case 30 :
+        echo ' You have developed a condition for completion of the course....';
+        break;                             
+}   
+?>
