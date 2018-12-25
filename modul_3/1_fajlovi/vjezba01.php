0c3fcb7d2b941d7bf151706a46235a8b10857695
Edin Jašarević <edinjasar14@gmail.com>
2018-12-26 00:06:28 +0100

    modul 03 | 1 fajlovi | vjezba 01

---
+++ modul_3/1_fajlovi/vjezba01.php
@@ -0,0 +1,16 @@
+<?php
+$file = "test"; 
+$validExtension = "php";
+switch( $validExtension )
+    {
+        case "php":
+            include $file . ".php";
+            break;
+        case "html":
+            include $file . ".html";
+            break;
+        case "js":
+            include $file . ".js";
+            break;
+    }
+?>
